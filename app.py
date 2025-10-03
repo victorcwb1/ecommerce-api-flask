fb29cb6e1c1e507d00c2f900ec62545f9d51ab93
Victor Assuncao <victor.assuncao.marins@gmail.com>
2025-10-03 16:31:05 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,98 @@
+# import
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+# modelagem
+# produto (id, name, price, description)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"})
+    return jsonify({"message": "Invalid product data"}), 400
+
+# Recuperar o produto da base de dados, verificar se o produto existe, se existe apagar da base de dados, se não existe retornar 404 not found
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product delete successfully"})
+    return jsonify({"message": "Invalid bot found"}), 400
+
+# retornar as informações do produto via ID, caso não ache retorna erro 404 not found
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"}), 404 
+
+# atualizar os dados dos produtos, caso não ache o produto erro 404 not found
+
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404 
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data ['name']
+
+    if 'price' in data:
+        product.price = data['price']
+    
+    if 'description' in data:
+        product.description = data['description']
+    
+    db.session.commit()
+    return jsonify({"message": "Product update successfully"})
+
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price
+        }
+        product_list.append(product_data)
+       
+    return jsonify(product_list)
+
+
+
+# set root route
+
+@app.route('/') 
+def hello_world():
+    return 'hello world'
+
+if __name__ == "__main__":
+    app.run(debug=True)
